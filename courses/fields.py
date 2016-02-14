19db4f31881025d1d9b19edfd8d1b3d2475e3314
choupijiang <zhangshenghu@gmail.com>
2016-02-14 18:19:11 +0800

    xx

--- courses/fields.py
+++ courses/fields.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
 class OrderField(models.PositiveIntegerField):
+    description = "Custom Order Field."
+
     def __init__(self, for_fields=None, *args, **kwargs):
         self.for_fields = for_fields
         super(OrderField, self).__init__(*args, **kwargs)
